0688cdfff6a445b6f666f33421c1542556ebe1ce
Connor Lane <connor.lane858@gmail.com>
2026-01-11 17:23:33 +0000

    minor fixes
    - remove sys.path manipulation (we should never need this, the project
      should be installed and importable with uv sync).
    - filter curated paths for one per sub. right now it includes all runs
      per sub.
    - use paths relative to the PPMI dataset root. **fmriprep data now
      assumed to be in PPMI/data/fmriprep (move or symlink)**
    - add scanner phase direction (AP, PA, LR, RL) to features
    - add age_bin to features
    - parse session a bit cleaner
    - add assert check on data length, all data should be long enough
    - pre-commit ruff fmt

--- datasets/PPMI/scripts/make_ppmi_dataset.py
+++ datasets/PPMI/scripts/make_ppmi_dataset.py
@@ -9,12 +9,6 @@ import datasets as hfds
 import numpy as np
 import pandas as pd
 
-
-# Add source folder to path if running from repo root
-ROOT = Path(__file__).parents[3]  # fmri-fm-eval/
-SOURCE = ROOT / "source"
-sys.path.append(str(SOURCE))
-
 import fmri_fm_eval.nisc as nisc
 import fmri_fm_eval.readers as readers
 
@@ -27,50 +21,58 @@ logging.getLogger("nibabel").setLevel(logging.ERROR)
 
 _logger = logging.getLogger(__name__)
 
+ROOT = Path(__file__).parents[1]
+
 SPLITS = ["train", "validation", "test"]
 
 # Resample all time series to fixed TR
+# PPMI TR is 1.0 (for AP/PA) or 2.5 (for LR/RL).
 TARGET_TR = 2.5
 # Keep first 120 TRs (5 mins) from each run
+# All PPMI runs are 10 min long. We could also consider keeping more data.
 MAX_NUM_TRS = 120
 
 
 def main(args):
-    outdir = ROOT / f"datasets/PPMI/ppmi.{args.space}.arrow"
+    outdir = ROOT / f"data/processed/ppmi.{args.space}.arrow"
     _logger.info("Generating dataset: %s", outdir)
     if outdir.exists():
         _logger.warning("Output %s exists; exiting.", outdir)
         return 1
 
     # load curated subjects
-    curated_df = pd.read_csv(
-        ROOT / "datasets/PPMI/metadata/PPMI_curated.csv",
-        dtype={"Subject": str}
-        )
+    curated_df = pd.read_csv(ROOT / "metadata/PPMI_curated.csv", dtype={"Subject": str})
 
-    curated_paths = open(
-        ROOT / "datasets/PPMI/metadata/PPMI_curated_paths.txt"
-        ).read().splitlines()
+    # load curated paths and get first run per subject
+    all_curated_paths = open(ROOT / "metadata/PPMI_curated_paths.txt").read().splitlines()
+    curated_paths = {}
+    for path in all_curated_paths:
+        sub_with_prefix = path.split("/")[0]
+        sub = sub_with_prefix.split("-")[1]
+        if sub not in curated_paths:
+            curated_paths[sub] = path
+    curated_paths = list(curated_paths.values())
 
     if args.space in {"a424", "mni", "mni_cortex"}:
         suffix = "_space-MNI152NLin6Asym_res-2_desc-preproc_bold.nii.gz"
     else:
         suffix = "_space-fsLR_den-91k_bold.dtseries.nii"
-        
+
     # preprocessed data paths
-    fmriprep_root = Path("datasets/PPMI/fmriprep")
+    # note, preprocessed fmriprep data should go in the dataset folder under data/
+    fmriprep_root = ROOT / "data/fmriprep"
     curated_paths = [fmriprep_root / p.replace("_bold.nii.gz", suffix) for p in curated_paths]
-    
+
     # mapping of subs to assigned splits
     sub_split_map = {sub: split for sub, split in zip(curated_df["Subject"], curated_df["split"])}
-    
+
     missing_paths = [p for p in curated_paths if not p.exists()]
     if missing_paths:
         _logger.error(f"Missing {len(missing_paths)} files. First few:")
         for p in missing_paths[:5]:
             _logger.error(f"  {p}")
         return 1
-    
+
     # data paths for each split
     path_splits = {split: [] for split in SPLITS}
     for path in curated_paths:
@@ -84,33 +86,38 @@ def main(args):
     dim = readers.DATA_DIMS[args.space]
 
     # define features
-    features = hfds.Features({
-        "sub": hfds.Value("string"),
-        "ses": hfds.Value("string"), 
-        "sex": hfds.Value("string"),
-        "age": hfds.Value("float32"),
-        "dx": hfds.Value("string"),
-        "path": hfds.Value("string"),
-        "n_frames": hfds.Value("int32"),
-        "tr": hfds.Value("float32"),
-        "bold": hfds.Array2D(shape=(None, dim), dtype="float16"),
-        "mean": hfds.Array2D(shape=(1, dim), dtype="float32"),
-        "std": hfds.Array2D(shape=(1, dim), dtype="float32"),
-    })
-    
-     # generate the datasets with huggingface. cache to a temp dir to save space.
+    features = hfds.Features(
+        {
+            "sub": hfds.Value("string"),
+            "ses": hfds.Value("string"),
+            "dir": hfds.Value("string"),
+            "sex": hfds.Value("string"),
+            "age": hfds.Value("float32"),
+            "age_bin": hfds.Value("string"),
+            "dx": hfds.Value("string"),
+            "path": hfds.Value("string"),
+            "n_frames": hfds.Value("int32"),
+            "tr": hfds.Value("float32"),
+            "bold": hfds.Array2D(shape=(None, dim), dtype="float16"),
+            "mean": hfds.Array2D(shape=(1, dim), dtype="float32"),
+            "std": hfds.Array2D(shape=(1, dim), dtype="float32"),
+        }
+    )
+
+    # generate the datasets with huggingface. cache to a temp dir to save space.
     with tempfile.TemporaryDirectory(prefix="huggingface-") as tmpdir:
         dataset_dict = {}
         for split, paths in path_splits.items():
             dataset_dict[split] = hfds.Dataset.from_generator(
                 generate_samples,
                 features=features,
-                gen_kwargs={"paths": paths, 
-                            "curated_df": curated_df, 
-                            "root": fmriprep_root, 
-                            "reader": reader, 
-                            "dim": dim
-                            },
+                gen_kwargs={
+                    "paths": paths,
+                    "curated_df": curated_df,
+                    "root": fmriprep_root,
+                    "reader": reader,
+                    "dim": dim,
+                },
                 num_proc=args.num_proc,
                 split=hfds.NamedSplit(split),
                 cache_dir=tmpdir,
@@ -129,36 +136,28 @@ def generate_samples(paths, *, curated_df, root, reader, dim):
         tr = float(sidecar_data["RepetitionTime"])
 
         # Extract subject and session from path
-        sub_with_prefix = str(path.relative_to(root)).split("/")[0]
-        sub = sub_with_prefix.split("-")[1]
-        
-        # Parse session from filename
         stem = path.name.split(".")[0]
-
-        ses = "unknown"
-        for item in stem.split("_"):
-            if item.startswith("ses-"):
-                ses = item.replace("ses-", "")
-                break
+        meta = dict(item.split("-") for item in stem.split("_") if "-" in item)
+        sub = meta["sub"]
 
         # Get demographics from curated_df
         row = curated_df[curated_df["Subject"] == sub].iloc[0]
         meta = {
-            "sub": sub, 
-            "ses": ses,
-            "sex": row["Sex"], 
-            "age": float(row["Age"]), 
-            "dx": row["Group"]
+            "sub": sub,
+            "ses": meta["ses"],
+            "dir": meta["dir"],
+            "sex": row["Sex"],
+            "age": float(row["Age"]),
+            "age_bin": row["age_bin"],
+            "dx": row["Group"],
         }
 
         series = reader(path)
-        series = nisc.resample_timeseries(series, tr=tr, new_tr=TARGET_TR, kind="linear")
+        series = nisc.resample_timeseries(series, tr=tr, new_tr=TARGET_TR, kind="pchip")
 
         T, D = series.shape
-        if T < MAX_NUM_TRS:
-            _logger.warning(f"Path {path} has too few TRs ({T}<{MAX_NUM_TRS}), skipping.")
-            continue
-        
+        assert T >= MAX_NUM_TRS, f"Path {path} has too few TRs ({T}<{MAX_NUM_TRS})"
+
         series = series[:MAX_NUM_TRS]
         series, mean, std = nisc.scale(series)
 
